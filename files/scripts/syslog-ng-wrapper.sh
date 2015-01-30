f7c67719b8aa4ae4c70ff2c947daa201852f4a03
Stephen Garlick <sgarlick@taximagic.com>
2015-01-30 16:39:10 -0500

    testing

--- files/scripts/syslog-ng-wrapper.sh
+++ files/scripts/syslog-ng-wrapper.sh
@@ -10,4 +10,4 @@ TAG=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 sed -i "s/TOKEN/$TOKEN/g" /etc/syslog-ng/conf.d/22-loggly.conf
 sed -i "s/TAG/$TAG/g" /etc/syslog-ng/conf.d/22-loggly.conf
 cat /etc/syslog-ng/conf.d/22-loggly.conf
-exec /usr/sbin/syslog-ng -F
+exec /usr/sbin/syslog-ng -F --no-caps
