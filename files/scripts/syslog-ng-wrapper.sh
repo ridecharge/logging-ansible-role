0ae9fed1234e1c3d464c04c628aba3673216e076
Stephen Garlick <sgarlick@taximagic.com>
2015-02-02 10:53:14 -0500

    rename env var

--- files/scripts/syslog-ng-wrapper.sh
+++ files/scripts/syslog-ng-wrapper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -z "$TOKEN" ]; then
-  echo "Missing \$TOKEN"
+if [ -z "$LOGGLY_TOKEN" ]; then
+  echo "Missing \$LOGGLY_TOKEN"
   exit 1
 fi
 
 TAG=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
-sed -i "s/TOKEN/$TOKEN/g" /etc/syslog-ng/conf.d/22-loggly.conf
+sed -i "s/LOGGLY_TOKEN/$LOGGLY_TOKEN/g" /etc/syslog-ng/conf.d/22-loggly.conf
 sed -i "s/TAG/$TAG/g" /etc/syslog-ng/conf.d/22-loggly.conf
 cat /etc/syslog-ng/conf.d/22-loggly.conf
 exec /usr/sbin/syslog-ng -F --no-caps
