dd489c42fe48934d777984c269def982f74d3a4b
Stephen Garlick <sgarlick@taximagic.com>
2015-02-12 11:15:12 -0500

    syslog verbose

--- files/scripts/syslog-ng-wrapper.sh
+++ files/scripts/syslog-ng-wrapper.sh
@@ -10,4 +10,4 @@ TAG=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 sed -i "s/LOGGLY_TOKEN/$LOGGLY_TOKEN/g" /etc/syslog-ng/conf.d/22-loggly.conf
 sed -i "s/TAG/$TAG/g" /etc/syslog-ng/conf.d/22-loggly.conf
 cat /etc/syslog-ng/conf.d/22-loggly.conf
-exec /usr/sbin/syslog-ng -F --no-caps
+exec /usr/sbin/syslog-ng -F --no-caps -V
